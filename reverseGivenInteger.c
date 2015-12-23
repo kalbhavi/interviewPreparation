9023661c53ca5431b07983089c9f460fdcee115e
Srinivas Kalbhavi <srinivas.kalbhavi@gmail.com>
2015-12-22 22:09:35 -0800

    Added another condition check for the given function

--- reverseGivenInteger.c
+++ reverseGivenInteger.c
@@ -1,6 +1,10 @@
+#define INT_MAX     2147483647
+#define INT_MIN     (-INT_MAX - 1)
+
 int ReverseInteger(int inputNum){
 	int negFlag = 1, temp = 0;
-	int reversedNum = 0, max = 2147483647;
+	int reversedNum = 0;
+    
 	if (inputNum < 0){
 		negFlag = -1;
 		inputNum = -inputNum;
@@ -9,7 +13,7 @@ int ReverseInteger(int inputNum){
 		temp = inputNum % 10;
         reversedNum = (reversedNum * 10) + temp; //reverse the number
 	}
-	if (reversedNum > max) //check if the number overflows
+	if (reversedNum > INT_MAX || reversedNum < INT_MIN) //check if the number overflows
 		return 0;
 	else
 		return (int)reversedNum * negFlag;
